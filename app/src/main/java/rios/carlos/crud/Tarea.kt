d44e6ccf353114f957e2eda241aa989731477c33
Lunatikera <carlitosjuven@gmail.com>
2025-07-02 15:57:59 -0700

    Push

---
+++ app/src/main/java/rios/carlos/crud/Tarea.kt
@@ -0,0 +1,11 @@
+package rios.carlos.crud
+
+data class Tarea(
+    var id:  String="",
+    var titulo: String="",
+    var descripcion: String="")
+{
+    fun toMap(): Map<String, String>{
+        return mapOf("titulo" to titulo, "descripcion" to descripcion)
+    }
+}
